2422c33b27c42e3b06dc7684fea731b8fd38f5e2
pfeatherstone <pfeatherstone@pf>
2024-10-13 14:21:47 +0100

    WIP

---
+++ msgpack.h
@@ -0,0 +1,185 @@
+#pragma once
+
+#include <cstdint>
+#include <limits>
+#include <utility>
+#include <endian.h>
+
+namespace msgpackcpp
+{
+    template<class Stream>
+    inline void serialize(Stream&& out, bool v)
+    {
+        const uint8_t format = v ? 0xc3 : 0xc2;
+        out((const char*)&format, 1);  
+    }
+    
+    template<class Stream>
+    inline void serialize(Stream&& out, uint8_t v)
+    {
+        if (v <= 0x7f)
+        {
+            // positive fixint (7-bit positive integer)
+            out((const char*)&v, 1);
+        }
+        else
+        {
+            // unsigned 8
+            constexpr uint8_t format = 0xcc;
+            out((const char*)&format, 1);
+            out((const char*)&v, 1);
+        }       
+    }
+
+    template<class Stream>
+    inline void serialize(Stream&& out, int8_t v)
+    {
+        if (v < -(1<<5))
+        {
+            // signed 8
+            constexpr uint8_t format = 0xd0;
+            out((const char*)&format, 1);
+            out((const char*)&v, 1);
+        }
+        else
+        {
+            // negative fixing (5-bit negative integer)
+            out((const char*)&v, 1);
+        }
+    }
+
+    template<class Stream>
+    inline void serialize(Stream&& out, uint16_t v)
+    {
+        if (v <= std::numeric_limits<uint8_t>::max())
+        {
+            serialize(std::forward<Stream>(out), (uint8_t)v);
+        }
+        else
+        {
+            constexpr uint8_t format = 0xcd;;
+            out((const char*)&format, 1);
+            v = htobe16(v);
+            out((const char*)&v, 2);
+        }
+    }
+
+    template<class Stream>
+    inline void serialize(Stream&& out, int16_t v)
+    {
+        // Positive
+        if (v >= 0)
+        {
+            // uint16_t
+            serialize(std::forward<Stream>(out), (uint16_t)v);
+        }
+        // negative
+        else
+        {
+            if (v >= std::numeric_limits<int8_t>::min())
+            {
+                // int8_t
+                serialize(std::forward<Stream>(out), (int8_t)v);
+            }
+            else
+            {
+                constexpr uint8_t format = 0xd1;
+                out((const char*)&format, 1);
+                v = htobe16(v);
+                out((const char*)&v, 2);
+            }
+        }
+    }
+
+    template<class Stream>
+    inline void serialize(Stream&& out, uint32_t v)
+    {
+        if (v <= std::numeric_limits<uint16_t>::max())
+        {
+            serialize(std::forward<Stream>(out), (uint16_t)v);
+        }
+        else
+        {
+            constexpr uint8_t format = 0xce;
+            out((const char*)&format, 1);
+            v = htobe32(v);
+            out((const char*)&v, 4);
+        }
+    }
+
+    template<class Stream>
+    inline void serialize(Stream&& out, int32_t v)
+    {
+        // Positive
+        if (v >= 0)
+        {
+            // uint32_t
+            serialize(std::forward<Stream>(out), (uint32_t)v);
+        }
+        // negative
+        else
+        {
+            if (v >= std::numeric_limits<int16_t>::min())
+            {
+                // int16_t
+                serialize(std::forward<Stream>(out), (int16_t)v);
+            }
+            else
+            {
+                constexpr uint8_t format = 0xd2;
+                out((const char*)&format, 1);
+                v = htobe32(v);
+                out((const char*)&v, 4); // Casting to a byte array is the same as writing out as big-endian
+            }
+        }
+    }
+
+    template<class Stream>
+    inline void serialize(Stream&& out, uint64_t v)
+    {
+        if (v <= std::numeric_limits<uint32_t>::max())
+        {
+            serialize(std::forward<Stream>(out), (uint32_t)v);
+        }
+        else
+        {
+            constexpr uint8_t format = 0xcf;
+            out((const char*)&format, 1);
+            v = htobe64(v);
+            out((const char*)&v, 8); // Casting to a byte array is the same as writing out as big-endian
+        }
+    }
+
+    template<class Stream>
+    inline void serialize(Stream&& out, int64_t v)
+    {
+        // Positive
+        if (v >= 0)
+        {
+            // uint64_t
+            serialize(std::forward<Stream>(out), (uint64_t)v);
+        }
+        // negative
+        else
+        {
+            if (v >= std::numeric_limits<int32_t>::min())
+            {
+                // int32_t
+                serialize(std::forward<Stream>(out), (int32_t)v);
+            }
+            else
+            {
+                constexpr uint8_t format = 0xd3;
+                out((const char*)&format, 1);
+                v = htobe64(v);
+                out((const char*)&v, 8); // Casting to a byte array is the same as writing out as big-endian
+            }
+        }
+    }
+
+    // template<class Stream>
+    // inline void serialize(Stream&& out, float v)
+    // {
+        
+    // }
+}
---
+++ test.cpp
@@ -0,0 +1,96 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <random>
+#include <vector>
+#include <limits>
+#include <msgpack.hpp>
+#include <doctest.h>
+#include "msgpack.h"
+
+template<class Byte, class Allocator>
+struct vector_sink
+{
+    std::vector<Byte, Allocator>& buf;
+
+    vector_sink(std::vector<Byte, Allocator>& buf_) : buf{buf_} {}
+
+    void write(const char* data, size_t len)
+    {
+        buf.insert(end(buf), data, data + len);
+    }
+
+    void operator()(const char* data, size_t len)
+    {
+        write(data, len);
+    }
+};
+
+const auto num_errors = [](const auto& buf1, const auto& buf2)
+{
+    int errors{0};
+    size_t i{0};
+    for (; i < std::min(buf1.size(), buf2.size()) ; ++i)
+        if (buf1[i] != buf2[i])
+            ++errors;
+    for(; i < std::max(buf1.size(), buf2.size()); ++i)
+        ++errors;
+    return errors;
+};
+
+template<class Int, class Generator>
+Int random_int(Generator& gen)
+{
+    return std::uniform_int_distribution<Int>{std::numeric_limits<Int>::min(), std::numeric_limits<Int>::max()}(gen);
+}
+
+TEST_SUITE("[MSGPACK]")
+{
+    TEST_CASE("arithmetic types")
+    {
+        std::mt19937 eng(std::random_device{}());
+
+        std::vector<uint8_t> buf1, buf2;
+
+        for (int repeat = 0 ; repeat < 1000 ; ++repeat)
+        {
+            const uint8_t  a = random_int<uint8_t>(eng);
+            const int8_t   b = random_int<int8_t>(eng);
+            const uint16_t c = random_int<uint16_t>(eng);
+            const int16_t  d = random_int<int16_t>(eng);
+            const uint32_t e = random_int<uint32_t>(eng);
+            const int32_t  f = random_int<int32_t>(eng);
+            const uint64_t g = random_int<uint64_t>(eng);
+            const int64_t  h = random_int<int64_t>(eng);
+
+            {
+                // using msgpack-c library
+                vector_sink sink{buf1};
+                msgpack::packer pack{&sink};
+                pack.pack(a);
+                pack.pack(b);
+                pack.pack(c);
+                pack.pack(d);
+                pack.pack(e);
+                pack.pack(f);
+                pack.pack(g);
+                pack.pack(h);
+            }
+
+            {
+                // using custom library
+                vector_sink sink{buf2};
+                msgpackcpp::serialize(sink, a);
+                msgpackcpp::serialize(sink, b);
+                msgpackcpp::serialize(sink, c);
+                msgpackcpp::serialize(sink, d);
+                msgpackcpp::serialize(sink, e);
+                msgpackcpp::serialize(sink, f);
+                msgpackcpp::serialize(sink, g);
+                msgpackcpp::serialize(sink, h);
+            }
+
+            REQUIRE(num_errors(buf1, buf2) == 0);
+            buf1.clear();
+            buf2.clear();
+        }
+    }
+}
