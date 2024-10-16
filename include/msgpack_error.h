1dbc7b468d8dd0584be9c8cb6ae19133e87bc5a3
pfeatherstone <pfeatherstone@pf>
2024-10-16 06:35:17 +0100

    Added error_code

---
+++ include/msgpack_error.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+#include <system_error>
+
+namespace msgpackcpp
+{
+    enum deserialization_error
+    {
+        OUT_OF_DATA = 1,
+        BAD_FORMAT  = 2
+    };
+
+    struct deserialization_error_category : std::error_category
+    {
+        const char* name() const noexcept override 
+        {
+            return "msgpack deserialization";
+        }
+
+        std::string message(int ev) const override
+        {
+            switch(static_cast<deserialization_error>(ev))
+            {
+            case OUT_OF_DATA: return "Ran out of data while deserializing";
+            case BAD_FORMAT:  return "Found bad format";
+            default:          return "Unrecognised error";
+            }
+        }
+    };
+
+    const deserialization_error_category& get_deserialization_error_category_singleton()
+    {
+        static deserialization_error_category singleton;
+        return singleton;
+    }
+
+    inline std::error_code make_error_code(deserialization_error ec)
+    {
+        return {static_cast<int>(ec), get_deserialization_error_category_singleton()};
+    }
+}
+
+namespace std
+{
+    template <>
+    struct is_error_code_enum<msgpackcpp::deserialization_error> : true_type {};
+}
