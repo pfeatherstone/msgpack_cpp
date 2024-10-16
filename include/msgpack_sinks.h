924652d432472338a977002b764bca029deea171
pfeatherstone <pfeatherstone@pf>
2024-10-16 06:35:27 +0100

    Added sources for deserialization

--- include/msgpack_sinks.h
+++ include/msgpack_sinks.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <cstring>
 #include <vector>
 #include <ostream>
+#include <istream>
+#include "msgpack_error.h"
 
 namespace msgpackcpp
 {
@@ -17,6 +20,27 @@ namespace msgpackcpp
     {
         return [&](const char* bytes, size_t nbytes) {
             out.write(bytes, nbytes);
+        };
+    }
+
+    template<class Byte, class Alloc, std::enable_if_t<sizeof(Byte) == 1, bool> = true>
+    auto source(const std::vector<Byte, Alloc>& buf)
+    {
+        size_t offset{0};
+        return [&buf, offset](char* bytes, size_t nbytes) mutable {
+            if ((buf.size() - offset) < nbytes)
+                throw std::system_error(OUT_OF_DATA);
+            std::memcpy(bytes, buf.data() + offset, nbytes);
+            offset += nbytes;
+        };
+    } 
+
+    inline auto source(std::istream& in)
+    {
+        return [&](char* bytes, size_t nbytes) {
+            in.read(bytes, nbytes);
+            if (in.gcount() != (long)nbytes)
+                throw std::system_error(OUT_OF_DATA);
         };
     }
 }
