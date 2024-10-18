f644cc5c008c1e2563b62c1da7554dff99fb3257
pfeatherstone <45853521+pfeatherstone@users.noreply.github.com>
2024-10-18 08:58:38 +0100

    No longer pass out as universal reference.

--- include/msgpack_describe.h
+++ include/msgpack_describe.h
@@ -10,23 +10,23 @@ namespace msgpackcpp
         class T,
         class D1 = boost::describe::describe_members<T, boost::describe::mod_any_access>
     >
-    inline void serialize(Stream&& out, const T& obj, bool as_map = false)
+    inline void serialize(Stream& out, const T& obj, bool as_map = false)
     { 
         if (as_map)
         {
-            serialize_map_size(std::forward<Stream>(out), boost::mp11::mp_size<D1>::value);
+            serialize_map_size(out, boost::mp11::mp_size<D1>::value);
 
             boost::mp11::mp_for_each<D1>([&](auto D) {
-                serialize(std::forward<Stream>(out), D.name);
-                serialize(std::forward<Stream>(out), obj.*D.pointer);
+                serialize(out, D.name);
+                serialize(out, obj.*D.pointer);
             });
         }
         else
         {
-            serialize_array_size(std::forward<Stream>(out), boost::mp11::mp_size<D1>::value);
+            serialize_array_size(out, boost::mp11::mp_size<D1>::value);
 
             boost::mp11::mp_for_each<D1>([&](auto D) {
-                serialize(std::forward<Stream>(out), obj.*D.pointer);
+                serialize(out, obj.*D.pointer);
             });
         }
     }
