e41b09c3d8785c8a42fcf1544da6bbe495b487c1
pfeatherstone <45853521+pfeatherstone@users.noreply.github.com>
2024-10-18 09:10:16 +0100

    Added BAD_SIZE

--- include/msgpack_error.h
+++ include/msgpack_error.h
@@ -8,7 +8,8 @@ namespace msgpackcpp
     enum deserialization_error
     {
         OUT_OF_DATA = 1,
-        BAD_FORMAT  = 2
+        BAD_FORMAT  = 2,
+        BAD_SIZE    = 3
     };
 
     struct deserialization_error_category : std::error_category
@@ -24,6 +25,7 @@ namespace msgpackcpp
             {
             case OUT_OF_DATA: return "Ran out of data while deserializing";
             case BAD_FORMAT:  return "Found bad format";
+            case BAD_SIZE:    return "Found bad size";
             default:          return "Unrecognised error";
             }
         }
