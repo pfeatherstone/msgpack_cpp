4d30bbb25360d3a25b0cb229a4bd5e08377275df
pfeatherstone <pfeatherstone@pf>
2025-11-22 18:44:02 +0000

    benchmark deserialize

--- bench/main.cpp
+++ bench/main.cpp
@@ -12,16 +12,47 @@ using namespace std::chrono_literals;
 using msgpackcpp::serialize;
 using msgpackcpp::deserialize;
 using msgpackcpp::sink;
+using msgpackcpp::source;
 
-template<class T, class Generator>
-T random(Generator& gen)
+template<class Byte, class Allocator>
+struct vector_sink
 {
-    if constexpr(std::is_same_v<T, char> || std::is_same_v<T, int8_t> || std::is_same_v<T, uint8_t>)
-        return std::uniform_int_distribution<int>{std::numeric_limits<T>::min(), std::numeric_limits<T>::max()}(gen);
-    else if constexpr(std::is_integral_v<T>)
-        return std::uniform_int_distribution<T>{std::numeric_limits<T>::min(), std::numeric_limits<T>::max()}(gen);
-    else if constexpr (std::is_floating_point_v<T>)
-        return std::uniform_real_distribution<T>{std::numeric_limits<T>::min(), std::numeric_limits<T>::max()}(gen);
+    std::vector<Byte, Allocator>& buf;
+    vector_sink(std::vector<Byte, Allocator>& buf_) : buf{buf_} {}
+    void write(const char* data, size_t len) {buf.insert(end(buf), data, data + len);}
+};
+
+namespace custom_namespace
+{
+    struct custom_struct
+    {
+        char        c;
+        int8_t      i8;
+        uint8_t     u8;
+        int16_t     i16;
+        uint16_t    u16;
+        int32_t     i32;
+        uint32_t    u32;
+        int64_t     i64;
+        uint64_t    u64;
+        float       f32;
+        double      f64;
+        std::string str;
+
+        MSGPACK_DEFINE(c, i8, u8, i16, u16, i32, u32, i64, u64, f32, f64, str)
+    };
+
+    BOOST_DESCRIBE_STRUCT(custom_struct, (), (c, i8, u8, i16, u16, i32, u32, i64, u64, f32, f64, str))
+
+    struct custom_struct2
+    {
+        std::vector<custom_struct>      array;
+        std::map<int, custom_struct>    map;
+        std::vector<uint8_t>            binary;
+        MSGPACK_DEFINE(array, map, binary)
+    };
+
+    BOOST_DESCRIBE_STRUCT(custom_struct2, (), (array, map, binary))
 }
 
 std::string make_string()
@@ -42,90 +73,45 @@ std::string make_string()
             "importance.";
 }
 
-template<class Byte, class Allocator>
-struct vector_sink
+template<class T>
+void random(T& obj, std::mt19937_64& gen)
 {
-    std::vector<Byte, Allocator>& buf;
-    vector_sink(std::vector<Byte, Allocator>& buf_) : buf{buf_} {}
-    void write(const char* data, size_t len) {buf.insert(end(buf), data, data + len);}
-};
-
-template <
-    class T,
-    class D1 = boost::describe::describe_members<T, boost::describe::mod_any_access>
->
-void serialize_msgpack_cxx(std::vector<uint8_t>& buf, const T& obj, bool as_map = false)
-{ 
-    vector_sink out{buf};
-    msgpack::packer pack{&out};
-
-    if (as_map)
-    {
-        pack.pack_map(boost::mp11::mp_size<D1>::value);
-        boost::mp11::mp_for_each<D1>([&](auto D) {
-            pack.pack(D.name);
-            pack.pack(obj.*D.pointer);
-        });
-    }
-    else
-    {
-        pack.pack_array(boost::mp11::mp_size<D1>::value);
-        boost::mp11::mp_for_each<D1>([&](auto D) {
-            pack.pack(obj.*D.pointer);
-        });
-    }
+    if constexpr(std::is_same_v<T, char> || std::is_same_v<T, int8_t> || std::is_same_v<T, uint8_t>)
+        obj = std::uniform_int_distribution<int>{std::numeric_limits<T>::min(), std::numeric_limits<T>::max()}(gen);
+    else if constexpr(std::is_integral_v<T>)
+        obj = std::uniform_int_distribution<T>{std::numeric_limits<T>::min(), std::numeric_limits<T>::max()}(gen);
+    else if constexpr (std::is_floating_point_v<T>)
+        obj = std::uniform_real_distribution<T>{std::numeric_limits<T>::min(), std::numeric_limits<T>::max()}(gen);
 }
 
-namespace custom_namespace
+void random(custom_namespace::custom_struct& data, std::mt19937_64& eng)
 {
-    struct custom_struct
-    {
-        char        c;
-        int8_t      i8;
-        uint8_t     u8;
-        int16_t     i16;
-        uint16_t    u16;
-        int32_t     i32;
-        uint32_t    u32;
-        int64_t     i64;
-        uint64_t    u64;
-        float       f32;
-        double      f64;
-        std::string str;
-    };
-
-    BOOST_DESCRIBE_STRUCT(custom_struct, (), (
-        c, i8, u8, i16, u16, i32, u32, i64, u64, f32, f64,
-        str
-    ))
-
-    struct custom_struct2
-    {
-        std::vector<custom_struct>      array;
-        std::map<int, custom_struct>    map;
-        std::vector<uint8_t>            binary;
-    };
-
-    BOOST_DESCRIBE_STRUCT(custom_struct2, (), (array, map, binary))
+    random(data.c, eng);
+    random(data.i8, eng);
+    random(data.u8, eng);
+    random(data.i16, eng);
+    random(data.u16, eng);
+    random(data.i32, eng);
+    random(data.u32, eng);
+    random(data.i64, eng);
+    random(data.u64, eng);
+    random(data.f32, eng);
+    random(data.f64, eng);
+    data.str = make_string();
 }
 
 int main()
 {
     std::mt19937_64 eng(std::chrono::high_resolution_clock::now().time_since_epoch().count());
 
-    custom_namespace::custom_struct data;
-    data.c      = random<char>(eng);
-    data.i8     = random<int8_t>(eng);
-    data.u8     = random<uint8_t>(eng);
-    data.i16    = random<int16_t>(eng);
-    data.u16    = random<uint16_t>(eng);
-    data.i32    = random<int32_t>(eng);
-    data.u32    = random<uint32_t>(eng);
-    data.i64    = random<int64_t>(eng);
-    data.u64    = random<uint64_t>(eng);
-    data.f32    = random<float>(eng);
-    data.f64    = random<double>(eng);
-    data.str    = make_string();
+    custom_namespace::custom_struct2 data;
+    data.array.resize(10);
+    for (auto& obj : data.array) random(obj, eng);
+    random(data.map[0], eng);
+    random(data.map[-42], eng);
+    random(data.map[100000], eng);
+    data.binary.resize(1024);
+    std::generate(begin(data.binary), end(data.binary), [&]{uint8_t v; random(v, eng); return v;});
 
     // Warmup
     std::vector<uint8_t> buf0;
@@ -133,19 +119,34 @@ int main()
     serialize(out, data);
 
     std::vector<uint8_t> buf1;
-    serialize_msgpack_cxx(buf1, data);
+    vector_sink out1(buf1);
+    msgpack::pack(out1, data);
     printf("buf0.size() %zu buf1.size() %zu\n", buf0.size(), buf1.size());
 
-    ankerl::nanobench::Bench().minEpochTime(100ms).epochs(50).run("msgpackcpp::serialize", [&] {
+    ankerl::nanobench::Bench().minEpochTime(100ms).epochs(20).run("msgpackcpp::deserialize", [&] {
+        custom_namespace::custom_struct2 obj;
+        auto in = source(buf0);
+        deserialize(in, obj);
+        // ankerl::nanobench::doNotOptimizeAway(d);
+    });
+
+    ankerl::nanobench::Bench().minEpochTime(100ms).epochs(20).run("msgpackcpp::serialize", [&] {
         buf0.clear();
         auto out = sink(buf0);
         serialize(out, data);
         // ankerl::nanobench::doNotOptimizeAway(d);
     });
 
-    ankerl::nanobench::Bench().minEpochTime(100ms).epochs(50).run("msgpack_c::serialize", [&] {
+    ankerl::nanobench::Bench().minEpochTime(100ms).epochs(20).run("msgpack_c::deserialize", [&] {
+        msgpack::object_handle oh = msgpack::unpack((const char*)buf1.data(), buf1.size());
+        custom_namespace::custom_struct2 obj = oh.get().as<custom_namespace::custom_struct2>();
+        // ankerl::nanobench::doNotOptimizeAway(d);
+    });
+
+    ankerl::nanobench::Bench().minEpochTime(100ms).epochs(20).run("msgpack_c::serialize", [&] {
         buf1.clear();
-        serialize_msgpack_cxx(buf1, data);
+        vector_sink out(buf1);
+        msgpack::pack(out, data);
         // ankerl::nanobench::doNotOptimizeAway(d);
     });    
 }
